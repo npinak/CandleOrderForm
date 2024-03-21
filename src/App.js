f934bcaf271984a9588ee3736e98238bdb965058
Pinak Nayak <77262741+npinak@users.noreply.github.com>
2024-03-20 19:55:46 -0700

    added extra client input fields and added validation for number of scents and quantity number

--- src/App.js
+++ src/App.js
@@ -14,20 +14,25 @@ import {
   Combobox,
 } from "monday-ui-react-core";
 
-//todo get values from the boxes -- shpping, email, phone, inscription
-//todo validation
-//todo update database when board is updated
+//todo get values from the boxes -- shipping, email, phone, inscription
+//todo validation for phone, email,
+//todo implement undo functionality if time
+//done todo update database when board is updated
 
 const monday = mondaySdk();
 monday.setApiVersion("2024-04");
 
 const App = () => {
-  const [context, setContext] = useState();
   const [boardID, setBoardID] = useState();
-  const [firstName, setFirstName] = useState();
-  const [lastName, setLastName] = useState();
-  const [quantity, setQuantity] = useState();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [quantity, setQuantity] = useState(null);
   const [scents, setScents] = useState([]);
+  const [inscription, setInscription] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
   const scentValues = [
     {
       value: 1,
@@ -63,33 +68,37 @@ const App = () => {
   useEffect(() => {
     monday.execute("valueCreatedForUser");
 
-    // monday.listen("context", (res) => {
-    //   setContext(res.data);
-    // }); // todo might not be needed
     monday.get("context").then((res) => {
       setBoardID(res.data.boardId);
     });
   }, []);
 
   const createNewEntry = async () => {
-    // if (scents.length !== 3) {
-    //   monday.execute("notice", {
-    //     message: "Please choose 3 scents",
-    //     type: "error",
-    //     timeout: 5000,
-    //   });
-    //   return;
-    // } ///todo re-enable
+    if (scents.length !== 3) {
+      monday.execute("notice", {
+        message: "Please choose 3 scents",
+        type: "error",
+        timeout: 5000,
+      });
+      return;
+    }
 
-    const inscription = "place holder";
-    // const shippingAddress = "place holder"; -- todo add if core functionality is done
+    if (quantity < 1) {
+      monday.execute("notice", {
+        message: "Please fill out order quantity",
+        type: "error",
+        timeout: 5000,
+      });
+      return;
+    }
+
     const orderReceivedDate = new Date().toLocaleDateString("en-CA");
     const orderStatus = 0;
     const scentProfiles = scents.map((scent) => scent.value);
 
     try {
       const createResponse = await monday.api(
-        `mutation { create_item (board_id:${boardID}, group_id:  "topics", item_name: "New order", column_values: \"{\\\"text5\\\":\\\"${inscription}\\\",\\\"text\\\":\\\"${firstName}\\\", \\\"text6\\\":\\\"${lastName}\\\",\\\"dropdown\\\":\\\"${scentProfiles}\\\",\\\"status\\\":\\\"${orderStatus}\\\",\\\"date_1\\\":\\\"${orderReceivedDate}\\\",\\\"numbers\\\":\\\"${quantity}\\\"}\"){id}}`
+        `mutation { create_item (board_id:${boardID}, group_id:  "topics", item_name: "New order", column_values: \"{\\\"text5\\\":\\\"${inscription}\\\",\\\"text\\\":\\\"${firstName}\\\",\\\"client_email\\\":\\\"${email}\\\",\\\"long_text\\\":\\\"${address}\\\",\\\"phone\\\":\\\"${phone}\\\" ,\\\"text6\\\":\\\"${lastName}\\\",\\\"dropdown\\\":\\\"${scentProfiles}\\\",\\\"status\\\":\\\"${orderStatus}\\\",\\\"date_1\\\":\\\"${orderReceivedDate}\\\",\\\"numbers\\\":\\\"${quantity}\\\"}\"){id}}`
       );
 
       const orderID = createResponse.data.create_item.id;
@@ -106,8 +115,6 @@ const App = () => {
             }
           }`);
 
-      console.log(newItemInfo.data.items[0]);
-
       await setDoc(doc(db, `${boardID}`, `${orderID}`), {
         data: newItemInfo.data.items[0],
         status: "Active",
@@ -120,66 +127,126 @@ const App = () => {
   return (
     <div className="App">
       <main id="main-section">
-        {/* <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            console.log("submitted123123");
-          }}
-          id="order-form"
-        >
-          <button></button> */}
         <section id="input-section">
-          <Box marginBottom={Box.marginBottoms.MEDIUM}>
-            <Flex>
-              <Box className="input">
-                <TextField
-                  title="First Name"
-                  placeholder="Enter Customer First Name"
-                  // requiredAsterisk={true}
-                  // required={true}
-                  onChange={(event) => {
-                    setFirstName(event);
-                  }}
-                  size={TextField.sizes.LARGE}
-                  type={TextField.types.TEXT}
-                  className="input"
-                />
-              </Box>
-              <Box className="input" marginX={Box.marginXs.XL}>
-                <TextField
-                  title="Last Name"
-                  placeholder="Enter Customer Last Name"
-                  // requiredAsterisk={true}
-                  // required={true}
-                  onChange={(event) => {
-                    setLastName(event);
-                  }}
-                  size={TextField.sizes.LARGE}
-                  type={TextField.types.TEXT}
-                  className="input"
-                />
-              </Box>
-              <Box className="input">
-                <TextField
-                  title="Quantity"
-                  placeholder="Enter Quantity"
-                  // requiredAsterisk={true}
-                  // required={true}
-                  onChange={(event) => {
-                    setQuantity(event);
-                  }}
-                  size={TextField.sizes.LARGE}
-                  type={TextField.types.NUMBER}
-                  className="input"
-                  //todo check how to validate number less than 1
-                  //todo check how to add required
-                />
-              </Box>
-            </Flex>
+          <Box marginBottom={Box.marginBottoms.LARGE}>
+            <Box marginBottom={Box.marginBottoms.MEDIUM}>
+              <Flex>
+                <Box className="input">
+                  <TextField
+                    title="First Name"
+                    placeholder="Enter Customer First Name"
+                    // requiredAsterisk={true}
+                    // required={true}
+                    onChange={(event) => {
+                      setFirstName(event);
+                    }}
+                    size={TextField.sizes.LARGE}
+                    type={TextField.types.TEXT}
+                    className="input"
+                  />
+                </Box>
+                <Box className="input" marginX={Box.marginXs.XL}>
+                  <TextField
+                    title="Last Name"
+                    placeholder="Enter Customer Last Name"
+                    // requiredAsterisk={true}
+                    // required={true}
+                    onChange={(event) => {
+                      setLastName(event);
+                    }}
+                    size={TextField.sizes.LARGE}
+                    type={TextField.types.TEXT}
+                    className="input"
+                  />
+                </Box>
+                <Box className="input">
+                  <TextField
+                    title="Quantity"
+                    placeholder="Enter Quantity"
+                    // requiredAsterisk={true}
+                    // required={true}
+                    onChange={(event) => {
+                      setQuantity(event);
+                    }}
+                    size={TextField.sizes.LARGE}
+                    type={TextField.types.NUMBER}
+                    className="input"
+                    //todo check how to validate number less than 1
+                    //todo check how to add required
+                  />
+                </Box>
+              </Flex>
+            </Box>
+
+            <Box marginBottom={Box.marginBottoms.MEDIUM}>
+              <Flex>
+                <Box className="input">
+                  <TextField
+                    title="Email"
+                    placeholder="Enter Email"
+                    // requiredAsterisk={true}
+                    // required={true}
+                    onChange={(event) => {
+                      console.log(event);
+                      setEmail(event);
+                    }}
+                    size={TextField.sizes.LARGE}
+                    type={TextField.types.TEXT}
+                    className="input"
+                  />
+                </Box>
+                <Box className="input" marginX={Box.marginXs.XL}>
+                  <TextField
+                    title="Phone"
+                    placeholder="Enter Phone Number"
+                    // requiredAsterisk={true}
+                    // required={true}
+                    onChange={(event) => {
+                      setPhone(event);
+                    }}
+                    size={TextField.sizes.LARGE}
+                    type={TextField.types.TEXT}
+                    className="input"
+                    //todo check how to validate number less than 1
+                    //todo check how to add required
+                  />
+                </Box>
+                <Box className="input">
+                  <TextField
+                    title="Inscription"
+                    placeholder="Enter Inscription"
+                    // requiredAsterisk={true}
+                    // required={true}
+                    onChange={(event) => {
+                      setInscription(event);
+                    }}
+                    size={TextField.sizes.LARGE}
+                    type={TextField.types.TEXT}
+                    className="input"
+                    //todo check how to validate number less than 1
+                    //todo check how to add required
+                  />
+                </Box>
+              </Flex>
+            </Box>
+            <Box>
+              <TextField
+                title="Address"
+                placeholder="Enter Address"
+                // requiredAsterisk={true}
+                // required={true}
+                onChange={(event) => {
+                  setAddress(event);
+                }}
+                size={TextField.sizes.LARGE}
+                type={TextField.types.TEXT}
+              />
+            </Box>
           </Box>
         </section>
         <section id="dropdown-section">
           <Dropdown
+            size={Dropdown.size.LARGE}
             ref={dropdownRef}
             placeholder={"Choose 3 scents"}
             options={scentValues}
@@ -190,13 +257,11 @@ const App = () => {
             multi
             multiline
           />
-          {/* </Box> */}
         </section>
         <section id="button-section">
           <Box marginTop={Box.marginTops.MEDIUM}>
             <Button
               onClick={() => {
-                // extract data from fields, save to data base, and add to form
                 createNewEntry();
               }}
             >
@@ -204,7 +269,6 @@ const App = () => {
             </Button>
           </Box>
         </section>
-        {/* </form> */}
       </main>
     </div>
   );
